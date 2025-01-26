618ebf1fcc92a7ba3d0cad9af20b950d05a0e191
louis-ngugi <louisngugi858@gmail.com>
2025-01-26 18:23:03 +0300

    update index.js, Home.js, routes.js,  & ErrorPage.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes.js"; // Import the routes from routes.js
 import Home from "./pages/Home";  // Home component that is already there
 import About from "./pages/About";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
+import ErrorPage from "./pages/ErrorPage";
 
 
 // Create the router with an array of route objects
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />  // Home component on the root path
-  },
-  {
-    path: "/about",
-    element: <About />  // About page
-  },
-  {
-    path: "/login",
-    element: <Login />  // Login page
-  },
-  {
-    path: "/profile/:id",  // This is your dynamic route for the user profile
-    element: <UserProfile />  //  UserProfile here
-  }
+const router = createBrowserRouter(routes, [
+    {
+        path: "/",
+        element: <Home />,
+        errorElement: <ErrorPage /> // Add errorElement for Home
+      },
+      {
+        path: "/about",
+        element: <About />,
+        errorElement: <ErrorPage /> // Add errorElement for About
+      },
+      {
+        path: "/login",
+        element: <Login />,
+        errorElement: <ErrorPage /> // Add errorElement for Login
+      },
+      {
+        path: "/profile/:id",
+        element: <UserProfile />,
+        errorElement: <ErrorPage /> // Add errorElement for UserProfile
+      }
 ]);
 
 // Pass the router to RouterProvider to make it available in your app
